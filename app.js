8c66328dbf41e35b2d09895e6871f25eb59de700
ViniciusR23 <rgdovinicius@estudante.unisa.br>
2025-03-05 20:39:47 -0300

    atualizando a lista de nomes na tela

--- app.js
+++ app.js
@@ -7,8 +7,20 @@ function adicionarAmigo() {
     
     if (nome !== "" && amigos.indexOf(nome) === -1) {
         amigos.push(nome);
+        atualizarLista();
         input.value = "";
     } else {
         alert("Nome inválido ou já está na lista!");
+    }
+}
+
+function atualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
     }
 }
