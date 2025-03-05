1aa3dd92793bc7a157bc4964a0d8a03ec2aca5af
ViniciusR23 <rgdovinicius@estudante.unisa.br>
2025-03-05 19:57:16 -0300

    verificando o nome digitado

--- app.js
+++ app.js
@@ -4,4 +4,11 @@ function adicionarAmigo() {
     let input = document.getElementById("amigo");
     let nome = input.value;
     nome = nome.trim();
+    
+    if (nome !== "" && amigos.indexOf(nome) === -1) {
+        amigos.push(nome);
+        input.value = "";
+    } else {
+        alert("Nome inválido ou já está na lista!");
+    }
 }
